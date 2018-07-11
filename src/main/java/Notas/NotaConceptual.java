df208fccb84ad26bb61c67aa4f3ab403cb88002c
Nicolas <abramlujan@gmail.com>
2018-07-11 20:49:32 -0300

    Arreglar +/- de notas conceptuales

--- src/main/java/Notas/NotaConceptual.java
+++ src/main/java/Notas/NotaConceptual.java
@@ -1,9 +1,9 @@
 package Notas;
 
 public enum NotaConceptual implements Nota {
-	EMenos("Excelente", 1), E("Excelente", 0), EMas("Excelente", -1), MBMenos("Muy Bien", 1), MB("Muy Bien", 0), MBMas(
-			"Muy Bien", -1), BMenos("Bien", 1), B("Bien", 0), BMas("Bien", -1), RMenos("Regular",
-					1), R("Regular", 0), RMas("Regular", -1), MMenos("Mal", 1), M("Mal", 0), MMas("Mal", -1);
+	EMenos("Excelente", -1), E("Excelente", 0), EMas("Excelente", 1), MBMenos("Muy Bien", -1), MB("Muy Bien", 0), MBMas(
+			"Muy Bien", 1), BMenos("Bien", -1), B("Bien", 0), BMas("Bien", 1), RMenos("Regular",
+					-1), R("Regular", 0), RMas("Regular", 1), MMenos("Mal", -1), M("Mal", 0), MMas("Mal", 1);
 
 	private final String str; 
 	private final int masMenos; // >0 => '+', 0 => '-', ==0 => ''
