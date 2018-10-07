6c65289212402fb6a5a001588d10a62e68d86f28
MauroSantander <mauro.santander94@gmail.com>
2018-10-07 01:13:31 -0300

    Ahora funciona el GET, trae los datos de la rest

--- src/main/java/domain/Alumno.java
+++ src/main/java/domain/Alumno.java
@@ -23,37 +23,32 @@ public class Alumno {
 	public Alumno() {
 		
 	}
-
 	public String getFirst_name() {
 		return first_name;
 	}
-
-	public void setFirst_name(String nombre) {
-		this.first_name = nombre;
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
 	}
-
 	public String getLast_name() {
 		return last_name;
 	}
-
-	public void setLast_name(String apellido) {
-		this.last_name = apellido;
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
 	}
-
 	public String getGithub_user() {
 		return github_user;
 	}
-
-	public void setGithub_user(String usuarioGitHub) {
-		this.github_user = usuarioGitHub;
+	public void setGithub_user(String github_user) {
+		this.github_user = github_user;
 	}
-
 	public int getCode() {
 		return code;
 	}
-
-	public void setCode(int legajo) {
-		this.code = legajo;
+	public void setCode(int code) {
+		this.code = code;
 	}
+
+	
+	
 
 }
--- src/main/java/domain/AlumnoService.java
+++ src/main/java/domain/AlumnoService.java
@@ -5,57 +5,35 @@ import javax.ws.rs.core.MediaType;
 import com.google.gson.Gson;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
 
 public class AlumnoService {
+    private static final String API_NOTITAS = "http://notitas.herokuapp.com";
+    private Client client;
+	private String bearerToken;
 	
-//	 private static final String API_NOTITAS = "http://notitas.herokuapp.com";
-	    private Client client;
-		private String bearerToken;
+	
+	public AlumnoService(String bearerToken){
+		this.bearerToken = bearerToken;
+		this.client = Client.create();
+	}
+	
+	public Alumno getAlumno(){
+		ClientResponse clientResponse= this.client
+        .resource(API_NOTITAS)
+        .path("student")
+        .header("Authorization", "Bearer " + this.bearerToken)
+        .accept(MediaType.APPLICATION_JSON)
+        .get(ClientResponse.class);
 		
-		public AlumnoService(String bearerToken){
-			this.bearerToken = bearerToken;
-			this.client = Client.create();
-		}
-		
-		public Alumno getAlumno(){
-			WebResource webResource = client
-					   .resource("http://notitas.herokuapp.com");			
-			
-			webResource.header("Authorization", bearerToken);
-			
-			webResource.path("student");
-			
-			ClientResponse response = webResource.accept("application/json")
-	                   .get(ClientResponse.class);
-			
-			
-			if (response.getStatus() != 200) {
-				   throw new RuntimeException("Failed : HTTP error code : "
-					+ response.getStatus());
-				}
-
-			String output = response.getEntity(String.class);
-			
-			Alumno alumno = new Gson().fromJson(output, Alumno.class);
+		if(clientResponse.getStatus()==200){
+			clientResponse.bufferEntity();
+			String jsonString = clientResponse.getEntity(String.class);
+			Alumno alumno = new Gson().fromJson(jsonString, Alumno.class);
 			return alumno;
-			
-//			ClientResponse clientResponse= this.client
-//	        .resource(API_NOTITAS)
-//	        .path("student")
-//	        .header("Authorization", "Bearer " + this.bearerToken)
-//	        .accept(MediaType.APPLICATION_JSON)
-//	        .get(ClientResponse.class);
-//			
-//			if(clientResponse.getStatus()==200){
-//				clientResponse.bufferEntity();
-//				String jsonString = clientResponse.getEntity(String.class);
-//				Alumno alumno = new Gson().fromJson(jsonString, Alumno.class);
-//				return alumno;
-//			}
-//			else { throw new RuntimeException("Failed : HTTP error code : "
-//						+ clientResponse.getStatus());
-//			}
-									
 		}
+		return new Alumno();
+	}
+	
+	
+
 }
